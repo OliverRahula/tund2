5f6ef5b1cad89c45a43a376f9ac9741790a7f68e
Oliver Rahula <oliverrahula@hotmail.com>
2015-09-11 13:20:45 +0300

    Login.php

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+
+//echo $_POST["email"];
+
+//Defineeri muutujad
+$email_error = "";
+$password_error = "";
+
+//kontrollin kas keegi vajutas nuppu
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	
+	
+	//echo "jah;"
+	
+	
+	// kas e-post on tühi
+	if(empty($_POST["email"])))
+	
+
+	//jah oli tühi
+	$email_error = "See väli on kohustuslik";
+	$password_error = "See väli on kohustuslik";
+}
+
+
+
+?>
+<html>
+<head>
+	<title> Login page </title>
+</head>
+
+	
+	
+	
+<body>
+
+	
+	
+	<form action="login.php" method="post">
+		<h2>Login</h2>
+		<input name="email" type="email" placeholder="E-post" > <br><br>
+		<input name="password" type="password" placeholder="Parool" > <br><br>
+		<input type="submit" placeholder="Logi sisse" > <br><br>		
+	</form>
+
+		
+	
+	<h2>Create user</h2>
+		
+		
+		
+		
+</body>
+
+
+</html>
